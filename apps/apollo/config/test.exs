58c14571d9cbbc26daa0f1b0473adc4fe3a8fcbf
Kaz Walker <me@kaz.codes>
2018-02-24 18:53:05 -0800

    Switch to Codeship instead of Travis.

--- apps/apollo/config/test.exs
+++ apps/apollo/config/test.exs
@@ -1,7 +1,16 @@
 use Mix.Config
 
 # Configure your database
-config :apollo, Apollo.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  database: "apollo_test",
-  pool: Ecto.Adapters.SQL.Sandbox
+if System.get_env("CI") == "true" do
+  config :apollo, Apollo.Repo,
+    adapter: Ecto.Adapters.Postgres,
+    hostname: System.get_env("POSTGRES_PORT_5432_TCP_ADDR"),
+    username: "postgres",
+    database: "apollo_test",
+    pool: Ecto.Adapters.SQL.Sandbox
+else
+  config :apollo, Apollo.Repo,
+    adapter: Ecto.Adapters.Postgres,
+    database: "apollo_test",
+    pool: Ecto.Adapters.SQL.Sandbox
+end
